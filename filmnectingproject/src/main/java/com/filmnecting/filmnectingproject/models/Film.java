a6b6515e4ef59df03f34b1541858b3cf4bee599f
David-MarCle <martinezclementedavid@gmail.com>
2024-04-16 10:35:42 +0200

    merge ok

--- filmnectingproject/src/main/java/com/filmnecting/filmnectingproject/models/Film.java
+++ filmnectingproject/src/main/java/com/filmnecting/filmnectingproject/models/Film.java
@@ -1,6 +1,10 @@
 package com.filmnecting.filmnectingproject.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "peliculas")
@@ -66,7 +70,7 @@ public class Film {
         
     }
   
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
